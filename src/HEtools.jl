cb89c5cddaf9d4db663a5bf5b59547b135bc87a2
Bastiaan Quast <bquast@gmail.com>
2023-11-27 16:32:44 -0600

    add GenUnif function

--- src/HEtools.jl
+++ src/HEtools.jl
@@ -1,9 +1,16 @@
 module HEtools
 
-export coefmod
+export coefmod, genunif
 import Polynomials
-function coefmod(p,m)
-           Polynomials.Polynomial(mod.(p[:],m))
+using Random
+
+function coefmod(p, m)
+    Polynomials.Polynomial(mod.(p[:], m))
+end
+
+function genunif(n, q)
+    coeffs = rand(1:q, n)  # Generate n random integers from 1 to q
+    Polynomials.Polynomial(coeffs)
 end
 
 end
