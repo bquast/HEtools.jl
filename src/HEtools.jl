75f1f9683d34b47ba7645ef927a89e2f92c127da
Bastiaan Quast <bquast@gmail.com>
2023-11-27 16:54:08 -0600

    remove the random part, try first without

--- src/HEtools.jl
+++ src/HEtools.jl
@@ -1,16 +1,10 @@
 module HEtools
 
-export coefmod, genunif
+export coefmod
 import Polynomials
-using Random
 
 function coefmod(p, m)
     Polynomials.Polynomial(mod.(p[:], m))
-end
-
-function genunif(n, q)
-    coeffs = rand(1:q, n)  # Generate n random integers from 1 to q
-    Polynomials.Polynomial(coeffs)
 end
 
 end
